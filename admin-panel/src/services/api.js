983a06f0cb177aee16723662d41248d1519e293c
habilshaikh <habilshaikh0023@gmail.com>
2026-08-23 17:01:29 +0530

    Use REACT_APP_API_URL env var for API base URL instead of hardcoded localhost

--- admin-panel/src/services/api.js
+++ admin-panel/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000/api' });
+const API = axios.create({ baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api' });
 
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem('admin_token');
