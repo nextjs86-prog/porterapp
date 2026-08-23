a074a8b1368caebe969f6eee7d1dd2eab8438b12
habilshaikh <habilshaikh0023@gmail.com>
2026-08-23 17:38:32 +0530

    Point mobile apps to deployed Render backend instead of localhost

--- driver-app/src/utils/api.js
+++ driver-app/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const BASE_URL = 'http://localhost:5000/api';
+const BASE_URL = 'https://porterapp-7y12.onrender.com/api';
 
 const api = axios.create({ baseURL: BASE_URL });
 
